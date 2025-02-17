6530ae54960661040336aa460f2598e5d67643c9
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-02-17 13:00:19 +0200

    upd

--- dist/js/pages/page-home.js
+++ dist/js/pages/page-home.js
@@ -1,4 +1,15 @@
 "use strict";
 (function (){
 	new DataTable('#example');
+	
+	$(".js-example-responsive2").select2({
+		width: 'resolve',
+		placeholder: 'Продаж',
+		minimumResultsForSearch: -1,
+	});
+	$(".js-example-responsive3").select2({
+		width: 'resolve',
+		placeholder: 'Валюта',
+		minimumResultsForSearch: -1,
+	});
 })();
