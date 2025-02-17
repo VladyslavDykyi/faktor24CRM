b60e662184eeb6a49d9e0ea3fa216b91f4a5b46a
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-02-17 13:10:51 +0200

    upd

--- #src/js/pages/page-home.js
+++ #src/js/pages/page-home.js
@@ -12,4 +12,45 @@
 		placeholder: 'Валюта',
 		minimumResultsForSearch: -1,
 	});
+	
+	$(document).ready(function() {
+		const $cityInput = $('#city');
+		const $listCountry = $('#list-country');
+		const $listOblast = $('#list-oblast');
+		const $listCity = $('#list-city');
+		const $listStreet = $('#list-street');
+		
+		$cityInput.on('input', function() {
+			if ($cityInput.val().trim() !== '') {
+				$listCountry.show();
+				$listOblast.hide();
+				$listCity.hide();
+				$listStreet.hide();
+			} else {
+				$listCountry.hide();
+			}
+		});
+		
+		$listCountry.find('input[type="radio"]').on('change', function() {
+			if ($(this).is(':checked')) {
+				$listOblast.show();
+				$listCity.hide();
+				$listStreet.hide();
+			}
+		});
+		
+		$listOblast.find('input[type="radio"]').on('change', function() {
+			if ($(this).is(':checked')) {
+				$listCity.show();
+				$listStreet.hide();
+			}
+		});
+		
+		$listCity.find('input[type="checkbox"]').on('change', function() {
+			if ($(this).is(':checked')) {
+				$listStreet.show();
+			}
+		});
+	});
+	
 })();
