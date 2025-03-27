45a07f3b2fa4873d753fd352b54ef912e3e6b3dc
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-03-27 23:28:26 +0200

    upd 6.7.3

--- dist/js/pages/filter1.js
+++ dist/js/pages/filter1.js
@@ -26,5 +26,18 @@
 			});
 		}, 100);
 	});
-	
+	$('.select2-container')
+		.off('touchstart')
+		.on('touchstart', function (e) {
+			e.stopPropagation();
+		})
+		.siblings('select')
+		.off('select2:open')
+		.on('select2:open', function () {
+			$('.select2-results__options')
+				.off('touchstart')
+				.on('touchstart', 'li', function (e) {
+					e.stopPropagation();
+				});
+		});
 })();
