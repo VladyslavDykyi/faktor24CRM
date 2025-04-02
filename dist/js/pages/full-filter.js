083033e674391b40892d7991987439d8d50fa7bb
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-04-02 20:39:03 +0300

    upd 6.8.1

--- dist/js/pages/full-filter.js
+++ dist/js/pages/full-filter.js
@@ -33,11 +33,9 @@ $('#datapiker1').daterangepicker({
 		],
 		"firstDay": 1
 	},
-	
 	"drops": "auto"
 });
 $(document).ready(function() {
-	
 	$('#full-filter-btn').on('click', function() {
 		// Додаємо або видаляємо клас `active` на кнопці
 		$(this).toggleClass('active');
@@ -52,7 +50,6 @@ $(document).ready(function() {
 		var currentState = $(this).attr('data-open-menu');
 		var newState = currentState === 'false' ? 'true' : 'false';
 		$(this).attr('data-open-menu', newState);
-		
 		if (newState === 'true') {
 			saveOriginalItems(); // Зберігаємо елементи при відкритті меню
 		}
@@ -68,13 +65,11 @@ $(document).ready(function() {
 		event.stopPropagation();
 	});
 	
-	
 	// Обробник для всіх чекбоксів
 	$(document).on('change', '.multiple-menu-list input[type="checkbox"]', function() {
 		const $currentList = $(this).closest('.multiple-menu-list'); // Поточний список
 		const $allCheckbox = $currentList.find('input[data-name="checkbox-all"]'); // Чекбокс "Все" в поточному списку
 		const $otherCheckboxes = $currentList.find('input[type="checkbox"]').not($allCheckbox); // Інші чекбокси
-		
 		if ($(this).data('name') === 'checkbox-all') {
 			// Якщо змінений чекбокс "Все"
 			if ($(this).is(':checked')) {
@@ -87,7 +82,6 @@ $(document).ready(function() {
 				// Якщо обрано інший чекбокс, знімаємо галочку з "Все"
 				$allCheckbox.prop('checked', false);
 			}
-			
 			// Перевіряємо, чи всі інші чекбокси не вибрані
 			if ($otherCheckboxes.filter(':checked').length === 0) {
 				// Якщо жоден інший чекбокс не вибрано, ставимо галочку на "Все"
