5dcdedf38ee34c4015c57bca663181ea993fe1f6
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-04-08 03:51:30 +0300

    upd 7.0.0

---
+++ dist/js/pages/page-create.js
@@ -0,0 +1,46 @@
+"use strict";
+(function () {
+	$(".js-example-responsive2-currency").select2({
+		width: 'resolve',
+		placeholder: 'Валюта',
+		minimumResultsForSearch: -1,
+	});
+	
+	$(".js-example-responsive2").select2({
+		width: 'resolve',
+		placeholder: 'Выбрать',
+		minimumResultsForSearch: -1,
+	});
+	
+	document.addEventListener('DOMContentLoaded', () => {
+		setTimeout(() => {
+			document.querySelectorAll('input, select').forEach(el => {
+				el.value = '';
+				el.autocomplete = 'new-password';
+			});
+		}, 100);
+	});
+	
+	$('.my-select2').on('select2:opening', function (e) {
+		$('.filter select').attr("style", "display: none !important");
+	});
+	
+	$('.my-select2').on('select2:closing', function (e) {
+		$('.filter select').attr("style", "display: block !important");
+	});
+	
+	
+		
+		
+		// Оновлення маски при зміні країни
+		// input.on("countrychange", function() {
+		// 	const countryData = input.intlTelInput("getSelectedCountryData");
+		// 	console.log("Обрана країна:", countryData.name, "Код:", countryData.dialCode);
+		//
+		// 	// Оновлюємо placeholder (необов'язково)
+		// 	const placeholder = `+${countryData.dialCode} (__) ___ - __ - __`;
+		// 	input.attr("placeholder", placeholder);
+		// });
+	
+	
+})();
