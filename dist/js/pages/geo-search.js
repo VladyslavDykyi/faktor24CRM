298f5dcfbd743e3c6dd1de9f99d802d7d32a86d0
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-06-18 23:48:32 +0300

    upd 7.7.0

--- dist/js/pages/geo-search.js
+++ dist/js/pages/geo-search.js
@@ -1,4 +1,24 @@
 'use strict';
+import { GoogleMapsManager  } from "./function_on_pages-create.min.js";
+
 (function () {
 
+// Usage example:
+	const mapsManager = new GoogleMapsManager();
+
+// Modal open handler
+	$('#geoModal').on('show.bs.modal', async function() {
+		try {
+			await mapsManager.loadAPI();
+			mapsManager.initializeMap('map-container');
+		} catch (error) {
+			console.error('Error:', error);
+			alert('Error: ' + error.message);
+		}
+	});
+
+// Modal close handler
+	$('#geoModal').on('hidden.bs.modal', function() {
+		mapsManager.clearFields();
+	});
 })();
