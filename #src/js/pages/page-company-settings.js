c9db18b79cecd862344384747b5c72ae53346b77
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2025-10-28 04:54:08 +0200

    upd 8.5.0

--- #src/js/pages/page-company-settings.js
+++ #src/js/pages/page-company-settings.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import {
-	PhoneInputManager, PhotoLoaderMini,
+	PhotoLoaderMini,
 } from "./function_on_pages-create.js";
 
 $("#select-company").select2({
@@ -14,49 +14,7 @@ $(".js-example-responsive2").select2({
 	width: 'resolve',
 	minimumResultsForSearch: -1,
 });
-
 new PhotoLoaderMini({
-	inputId: 'loading-photo',
-	wrapperClass: 'photo-info-list'
-});
-
-new PhoneInputManager({
-	btnSelector: '.btn-new-tel',
-	wrapperSelector: '.block-row .phone',
-	inputClass: 'tel-contact',
-	maxPhones: 5,
-	initialCountry: 'ua',
-	utilsScript: 'https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.1/build/js/utils.js',
-	countryMasks: {
-		'ua': '(99) 999-99-99',
-		'us': '(999) 999-9999',
-		'gb': '9999 999999',
-		'de': '999 99999999',
-		'fr': '9 99-99-99-99',
-		'pl': '999 999-999',
-		'it': '999 999-9999',
-		'es': '999 99-99-99',
-		'default': '(999) 999-99-99'
-	},
-	// якщо треба щось дописати то треба дописувати class де зовнішні змінни передавання ззовні(звідси)
-});
-new PhoneInputManager({
-	btnSelector: '.btn-new-tel',
-	wrapperSelector: '.block-row .company-branch-phone',
-	inputClass: 'tel-contact',
-	maxPhones: 5,
-	initialCountry: 'ua',
-	utilsScript: 'https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.1/build/js/utils.js',
-	countryMasks: {
-		'ua': '(99) 999-99-99',
-		'us': '(999) 999-9999',
-		'gb': '9999 999999',
-		'de': '999 99999999',
-		'fr': '9 99-99-99-99',
-		'pl': '999 999-999',
-		'it': '999 999-9999',
-		'es': '999 99-99-99',
-		'default': '(999) 999-99-99'
-	},
-	// якщо треба щось дописати то треба дописувати class де зовнішні змінни передавання ззовні(звідси)
+	inputIdSelector: '#loading-photo',
+	wrapperClassSelector: '.photo-info-list',
 });
